d59ba6909f68c135d356788c0bae881402ae95ad
nrdstrm <robert@nrdstrm.se>
2018-08-21 21:42:16 +0200

    TEA - Effects - Time #1

---
+++ src/elm/Effects - Time #1.elm
@@ -0,0 +1,83 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Time exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { time : Time
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model 0, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Tick Time
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick time ->
+            ( { model | time = time }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every second Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ clock model
+        ]
+
+
+clock : Model -> Html Msg
+clock model =
+    let
+        angle =
+            turns (Time.inMinutes model.time)
+
+        handX =
+            toString (50 + 40 * cos angle)
+
+        handY =
+            toString (50 + 40 * sin angle)
+    in
+        svg [ viewBox "0 0 100 100", Svg.Attributes.width "300px" ]
+            [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+            , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+            ]
